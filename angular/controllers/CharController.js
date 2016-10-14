982215489592820f3a4caf201b94c5a44b3cd3c2
Ruben Paz <me@ruben.io>
2016-10-14 21:52:57 +0200

    Clean ctrlloaders

--- angular/controllers/CharController.js
+++ angular/controllers/CharController.js
@@ -1,5 +1,5 @@
 
-app.controller('CharController', [ '$scope', '$http', function($scope, $http) {
+function CharController($scope, $http) {
 
   var url = 'http://api.guildwars2.com/v2/characters';
   $scope.APIKEY = 'F86BE23F-FCF1-FA42-A9F1-0D7BE7880214F63BD4B5-D2D4-44B1-86FE-5733FBF3B9A9';
@@ -16,7 +16,9 @@ app.controller('CharController', [ '$scope', '$http', function($scope, $http) {
       console.log("hiya");
     });
   };
-}]);
+};
+
+CharController.$inject = ['$scope', '$http'];
 
 
 /*
