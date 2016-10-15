6808903b83f86c2f6a54ef8607ff4efb60bd7365
Martin Rubio <martinrubio23@gmail.com>
2016-10-15 10:45:53 +0200

    Better controller and dao service implemented. Character Menu working nice.

--- angular/controllers/CharController.js
+++ angular/controllers/CharController.js
@@ -1,39 +1,19 @@
 
-app.controller('CharController', [ '$scope', '$http', function($scope, $http) {
+app.controller('CharController', [ '$scope', '$http', 'dao',function($scope, $http, dao) {
 
   var url = 'http://api.guildwars2.com/v2/characters';
   $scope.APIKEY = 'F86BE23F-FCF1-FA42-A9F1-0D7BE7880214F63BD4B5-D2D4-44B1-86FE-5733FBF3B9A9';
   //$scope.chars = 'null';
-  $scope.getChars = function(){
-    $http.get(url+'?access_token='+$scope.APIKEY).then(function(response){
-      $scope.chars = response.data;
+  dao.getChars().then( function(chrs){
+      $scope.chars = chrs;
+  });
+
+  $scope.getChar = function(name) {
+    dao.getCharsByName(name).then(function(chr) {
+      console.log(chr);
+      $scope.char = chr;
     });
   };
 
-  $scope.getChar = function(name){
-    $http.get(url+'/'+name+'?access_token='+$scope.APIKEY).then(function(response){
-      $scope.char = response.data;
-      console.log("hiya");
-    });
-  };
+
 }]);
-
-
-/*
-
-<ul>
-  <li>
-    <img width="40px" src="imgs/profesions/engineer.png">
-    <a href="#char1">Character 1.</a>
-  </li>
-  <li>
-    <img width="40px" src="imgs/profesions/thief.png">
-    <a href="#char1">Character 2.</a>
-  </li>
-  <li>
-    <img width="40px" src="imgs/profesions/warrior.png">
-    <a href="#char1">Character 3.</a>
-  </li>
-</ul>
-
-*/
---
+++ angular/services/dao.js
@@ -0,0 +1,20 @@
+
+app.service('dao', function($http, $q){
+
+  var url = 'http://api.guildwars2.com/v2/characters';
+  APIKEY = 'F86BE23F-FCF1-FA42-A9F1-0D7BE7880214F63BD4B5-D2D4-44B1-86FE-5733FBF3B9A9';
+
+
+
+  function getCharsByName(name) {
+    return $http.get(url+'/'+name+'?access_token='+APIKEY).then(function(response){
+      return $q.all(response.data);
+    });
+  };
+
+  this.getChars = function(){
+    return $http.get(url+'?access_token='+APIKEY).then(function(response){
+      return $q.all(response.data.map(getCharsByName));
+    });
+  };
+});
